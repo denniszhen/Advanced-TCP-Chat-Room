f3f0288aeea2d6e8fbdf3ee9b66121e4abb7bba5
Dennis Zhen <54695660+denniszhen@users.noreply.github.com>
2022-02-23 12:15:25 -0500

    Added client-side admin functionality (kick/ban)

--- client.py
+++ client.py
@@ -3,6 +3,9 @@ import threading
 
 # Choosing Nickname
 nickname = input("Choose your nickname: ")
+if nickname == 'admin':
+    password = input("Enter admin password")
+
 
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,12 +16,24 @@ client.connect(('127.0.0.1', 8080))
 
 def receive():
     while True:
+        global stop_thread
+        if stop_thread:
+            break
         try:
             # Receive Message From Server
             # If 'NICK' Send Nickname
             message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
+            if message == 'NICK':  # Get asked for the nickname
+                # Send the nickname to the server
                 client.send(nickname.encode('ascii'))
+                next_message = client.recv(1024).decode('ascii')
+                if next_message == 'PASS':  # If server requests password because your nickname was 'admin'
+                    # Send the password to the server
+                    client.send(password.encode('ascii'))
+                    # If the password was wrong
+                    if client.recv(1024).decode('ascii') == 'REFUSE':
+                        print("Connection refused. Wrong password.")
+                        stop_thread = True  # Stop the thread
             else:
                 print(message)
         except:
@@ -32,8 +47,27 @@ def receive():
 
 def write():
     while True:
+        if stop_thread:  # Break the write loop if the global stop_thread variable is true
+            break
         message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        # Escape the username, colon, and space to see if a command is being executed
+        if message[len(nickname)+2].startswith('/'):
+            if nickname == 'admin':  # if admin user
+                if message[len(nickname)+2].startswith('/kick'):  # if kick command is entered
+                    # send kick command to the server
+                    client.send(
+                        'KICK {message[len(nickname)+2+6]}'.encode('ascii'))
+                # if ban command is entered
+                elif message[len(nickname)+2].startswith('/ban'):
+                    # send ban command to the server
+                    client.send(
+                        'BAN {message[len(nickname)+2+5]}'.encode('ascii'))
+            else:
+                # if command was entered by non-admin user
+                print("Commands can only be executed by admins")
+        else:
+            # if the message wasn't a command to begin with, send it normally
+            client.send(message.encode('ascii'))
 
 
 # Starting Threads For Listening And Writing
